36e123306a697c904f51ea7716549e24da0d1c76
Sery0077 <sery.vlasenko.ru@gmail.com>
2023-01-22 14:51:48 +0700

    add navigation

--- app/src/main/java/sery/vlasenko/recorder/MainActivity.kt
+++ app/src/main/java/sery/vlasenko/recorder/MainActivity.kt
@@ -1,11 +1,21 @@
 package sery.vlasenko.recorder
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        NavigationUI.setupWithNavController(
+            bottomNavigation,
+            findNavController(R.id.nav_host_fragment_container)
+        )
     }
 }
